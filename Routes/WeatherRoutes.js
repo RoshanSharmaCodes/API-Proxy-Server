53bba1015f4a116b6709e66d71ee56938ae9f078
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2023-03-16 23:31:13 +0530

    * Added Rate Limit to API.

--- Routes/WeatherRoutes.js
+++ Routes/WeatherRoutes.js
@@ -1,19 +1,20 @@
+const url = require("url")
 const express = require("express")
 const weatherRoute = express.Router()
 const needle = require("needle")
+require("dotenv").config()
 
 //constants
 const API_BASE_URL = process.env.API_BASE_URL
 const API_BASE_KEY = process.env.API_BASE_KEY
 const API_BASE_NAME = process.env.API_BASE_NAME
 
-weatherRoute.get("/", async (req, res) => {
-  try{
-    const param = new URLSearchParams({[API_BASE_NAME]:API_BASE_KEY})
-    const weatherRes = await needle("get",`${API_BASE_URL}?${param}`)
-  }catch{
-
-  }
+weatherRoute.get("/", async (req, res, next) => {
+  try {
+    const param = new URLSearchParams({ [API_BASE_NAME]: API_BASE_KEY, ...url.parse(req.url, true).query })
+    const weatherRes = await needle("get", `${API_BASE_URL}?${param}`)
+    next()
+  } catch {}
 })
 
 //to handle loose paths
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const weatherRoute = require("./Routes/WeatherRoutes")
 const cors = require("cors")
+const rateLimit = require("express-rate-limit")
 require("dotenv").config()
 
+const limit = rateLimit({windowMs:5*60*1000, max:5})
 const PORT = process.env.PORT_NUMBER || 3000
 const app = express()
 
 app.use(cors())
+app.use(limit)
+app.set("trust proxy")
 
 app.get("/", (req, res) => {
   res.send("Code: 200 =>  Proxy Server is Running")
